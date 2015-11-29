bcb90c05df8c6cc7a8436920f89575a1d4a22e92
xcv58 <chenyihonglove@gmail.com>
2015-11-28 19:14:19 -0500

    print interval length

--- interval.py
+++ interval.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import json
+import time
+import datetime
 
 if len(sys.argv) == 1:
     print('no argument')
@@ -22,7 +24,7 @@ for i in f:
 print('query time', [i['end'] - i['start'] if 'suc' in i['status'] else -1 for i in result],
       len([i['end'] - i['start'] if 'suc' in i['status'] else -1 for i in result]))
 interval = [(i['start'] - j ['start']) for i, j in zip(result[1:], result[:-1])]
-print('interval', [i / 1000 for i in interval])
+print('interval', len(interval), [i / 1000 for i in interval])
 short = [i for i in interval if abs(i) < 3000]
 print('interval less than 3 sec', short)
 print(len(short), len(interval))
